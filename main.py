742f2263db3b3041862344923d8ef21ddacfd833
guthubsuteado <github.suteado@gmail.com>
2024-11-07 15:08:35 +0900

    schema-test

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from routers import contact
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "hello world"}
+# @app.get("/")
+# async def root():
+#     return {"message": "hello world"}
+
+app.include_router(contact.router)
